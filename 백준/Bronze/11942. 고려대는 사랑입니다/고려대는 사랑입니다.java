1f222498abb6d92e89f4313553db93d49d941e6d
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-12 17:58:53 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("고려대학교");
+	}
+}
