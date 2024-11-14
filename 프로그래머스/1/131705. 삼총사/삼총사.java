559811acce9c486c319abe6e189fc56513234d39
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-14 21:46:10 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0, m = number.length;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < m; j++) {
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = i + 1; j < m - 1; j++) {
                 for (int k = j + 1; k < m; k++) {
                     if (number[i] + number[j] + number[k] == 0) {
                         answer++;
