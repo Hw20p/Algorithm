25b494d53530d5352615d0b8d5187cfde1daa158
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-14 22:15:26 +0900

    [Bronze IV] Title: Hello Judge, Time: 136 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 1; i <= n; i++) {
+        	System.out.printf("Hello World, Judge %d!\n", i);
+        }
+    }
+}
