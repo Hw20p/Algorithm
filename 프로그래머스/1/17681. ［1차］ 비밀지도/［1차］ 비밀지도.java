572f119dc30b64c1ab4ede3c4e74bbc6a508182c
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-15 18:10:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.93 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,15 +1,27 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String format = "%" + n + "s";
+        String[] arr11 = new String[n];
+        String[] arr22 = new String[n];
+        String format = "%0" + n + "d";
+        int idx = 0;
+        
+        for (int i : arr1) {
+            arr11[idx++] = String.format(format, Integer.parseInt(Integer.toString(i, 2)));
+        }
+        
+        idx = 0;
+        for (int i : arr2) {
+            arr22[idx++] = String.format(format, Integer.parseInt(Integer.toString(i, 2)));
+        }
         
         for (int i = 0; i < n; i++) {
-            String arr11 = String.format(format, Integer.toBinaryString(arr1[i]));
-            String arr22 = String.format(format, Integer.toBinaryString(arr2[i]));
             StringBuilder sb = new StringBuilder();
-            
             for (int j = 0; j < n; j++) {
-                if (arr11.charAt(j) == '1' || arr22.charAt(j) == '1') {
+                int num11 = arr11[i].charAt(j) - '0';
+                int num22 = arr22[i].charAt(j) - '0';
+                
+                if (num11 == 1 || num22 == 1) {
                     sb.append("#");
                 } else {
                     sb.append(" ");
