cfff0e3784ca7d578e402d9b24c703dd9fda4a1d
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-20 09:19:04 +0900

    [level 1] Title: 2016년, Time: 1.65 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date = LocalDate.of(2016, a, b);
+        answer = date.getDayOfWeek().toString().substring(0, 3);
+        
+        return answer;
+    }
+}
