439af4c532cd5be0d5ced64b379764ec3e655a6a
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-26 09:30:28 +0900

    [level 1] Title: 예산, Time: 1.33 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,14 +3,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
-        int answer = 0, len = d.length;
-        
+        int answer = 0, sum = 0;
         Arrays.sort(d);
         
-        int sum = 0;
         for (int i : d) {
             sum += i;
-            
             if (sum > budget) {
                 break;
             } else {
