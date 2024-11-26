589292d564d95ac31d3420645196e064b30db631
Hw20p <155079284+Hw20p@users.noreply.github.com>
2024-11-26 15:07:46 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -4,8 +4,8 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        
         int[] arr = new int[2];
+        
         List<Integer> list = new ArrayList<>();
         for (int i : lottos) {
             if (i == 0) {
@@ -19,19 +19,13 @@ class Solution {
                 arr[1]++;
             }
         }
+        
         arr[0] += arr[1];
+        answer[0] = 6;
+        answer[1] = 6;
         
-        if (arr[0] > 1) {
-            answer[0] = 7 - arr[0];
-        } else {
-            answer[0] = 6;
-        }
-        
-        if (arr[1] > 1) {
-            answer[1] = 7 - arr[1];
-        } else {
-            answer[1] = 6;
-        }
+        if (arr[0] > 1) answer[0] = 7 - arr[0];
+        if (arr[1] > 1) answer[1] = 7 - arr[1];
 
         return answer;
     }
