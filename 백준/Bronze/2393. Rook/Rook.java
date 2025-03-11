b40559899b2354db27d48a6279011d8caf66835b
Hyeon <155079284+Hw20p@users.noreply.github.com>
2025-03-11 20:47:22 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("  ___  ___  ___\n"
+				+ "  | |__| |__| |\n"
+				+ "  |           |\n"
+				+ "   \\_________/\n"
+				+ "    \\_______/\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |_____|\n"
+				+ "  __/       \\__\n"
+				+ " /             \\\n"
+				+ "/_______________\\");
+	}
+}
