f4fce3e23cc02be92594526890ba0463bdafe88f
Hyeon <155079284+Hw20p@users.noreply.github.com>
2025-03-31 22:15:15 +0900

    [Bronze V] Title: WARBOY, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int answer = input[1] / input[0] * input[2] * 3;
+		
+		System.out.println(answer);
+	}
+}
