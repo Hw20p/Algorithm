af65525abb2a8882dca01f959b9f2fe16798d6c7
Hyeon <155079284+Hw20p@users.noreply.github.com>
2025-04-27 10:50:26 +0900

    [Bronze V] Title: Call for Problems, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/32498. Call for Problems/Call for Problems.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num % 2 != 0) cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
