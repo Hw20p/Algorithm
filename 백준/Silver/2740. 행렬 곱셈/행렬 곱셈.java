156310459b3ae94e8d97096ee0d53d352b00e315
Hyeon <155079284+Hw20p@users.noreply.github.com>
2025-04-28 15:55:35 +0900

    [Silver V] Title: 행렬 곱셈, Time: 300 ms, Memory: 19412 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 행렬 A
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr1 = new int[n][m];
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 행렬 B
+		st = new StringTokenizer(br.readLine());
+		int m2 = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] arr2 = new int[m2][k];
+		
+		for (int i = 0; i < m2; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < k; j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 행렬 A * B
+		int[][] arr3 = new int[n][k];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k; j++) {
+				for (int l = 0; l < m; l++) {
+					arr3[i][j] += arr1[i][l] * arr2[l][j];
+				}
+			}
+		}
+		
+		// 출력
+		for (int[] i : arr3) {
+			for (int j : i) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
