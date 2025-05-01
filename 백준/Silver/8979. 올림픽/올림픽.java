070c4b3dc2838cf104823ba6657dcfb1a8b6c952
Hyeon <155079284+Hw20p@users.noreply.github.com>
2025-05-01 16:28:01 +0900

    [Silver V] Title: 올림픽, Time: 128 ms, Memory: 14744 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -27,6 +27,11 @@ public class Main {
 	
 		
 		int rank = 1;
+		if (arr[0][0] == k) {
+			System.out.println(rank);
+			return;
+		}
+		
 		for (int i = 1; i < n; i++) {
 			if (arr[i][1] != arr[i - 1][1] ||
 		        arr[i][2] != arr[i - 1][2] ||
